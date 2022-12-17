0b26a41c1fe49593eb2b27c533ec609f0267c1e8
Suvetha-A <swethaarivazhagan02@gmail.com>
2022-12-17 14:27:43 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Title from './Component/title';
+
+
+const App=()=> {
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
